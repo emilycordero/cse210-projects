1d6d0b551cb4f38b5c418273927867ce2544c131
Emily Cordero <cordeemi031@gmail.com>
2023-11-16 19:02:46 -0800

    added new files

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel.Design;
+// Consider an app that provides three different kinds of mindfulness opportunities. It could give some guidance and structure to users in the following activities:
 
+//Breathing Activity - Help the user pace their breathing to have a session of deep breathing for a certain amount of time. They might find more peace and less stress through the exercise.
+//Reflection Activity - Guide the user to think deeply, by having them consider a certain experience when they were successful or demonstrated strength. Then, prompt them with questions to reflect more deeply about details of this experience. They might discover more depth than they previously realized.
+//Listing Activity - Guide the user to think broadly, by helping them list as many things as they can in a certain area of strength or positivity. They might discover more breadth than they previously realized.
+//The application could additional help the user keep track of the time or frequency they spend in these activities and give them gentle prompts and reminders.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while (true)
+        {
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Start breathing activity\n2. Start reflecting activity\n3. Start listing activity\n4. Quit");
+            Console.WriteLine("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Welcome to the Breathing Activity.")
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
