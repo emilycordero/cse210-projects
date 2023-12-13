ab8a916011863f491fb716744e1266bde8f0d951
Emily Cordero <cordeemi031@gmail.com>
2023-12-12 23:08:21 -0800

    added final changes ready for push

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -5,6 +5,7 @@ public class Lecture : Event
     private string speakerName;
     private int capacity;
 
+    public Lecture(string speakerName, int capacity, string eventName, string eventDescription, DateTime eventDate, TimeSpan eventTime, Address eventAddress) : base(eventName, eventDescription, eventDate, eventTime, eventAddress)
     public Lecture(string eventName, string eventDescription, string eventCategory, DateTime eventDate, TimeSpan eventTime, Address eventAddress, string speakerName, int capacity) : base(eventName, eventDescription, eventCategory, eventDate, eventTime, eventAddress) 
     {
         this.speakerName = speakerName;
@@ -12,14 +13,14 @@ public class Lecture : Event
 
     }
 
-    public string GenerateStandardDetails()
+    public override string GenerateFullDetails()
     {
-    return "Join us at " + EventName + " on " + EventDate + " at " + EventTime + ". We will be meeting in " + EventAddress + " for " + EventDescription + ".";
+    return "Join us at " + eventName + " on " + eventDate + " at " + eventTime + ". We will be meeting in " + eventAddress + " for " + eventDescription + ".";
     }
 
     public virtual string GenerateFullDetails()
     {
-    return "Join us at " + EventName + " on " + EventDate + " at " + EventTime + " for a " + EventCategory + ". We will be meeting in " + EventAddress + " for " + EventDescription + ". Speaking today will be: " + speakerName + ". Max capacity: " + capacity + " people.";
+    return "Join us at " + eventName + " on " + eventDate + " at " + eventTime + ". We will be meeting in " + eventAddress + " for " + eventDescription + ".";
     }
 
     public virtual string GenerateShortDescription()
